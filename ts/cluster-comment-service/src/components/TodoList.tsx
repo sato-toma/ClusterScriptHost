30084b812c827b1fb16eeea8d766df6c72c67a48
sato-toma <h.k.tomate@gmail.com>
2025-07-26 17:54:03 +0900

    add update and remove todo at TodoList component

--- ts/cluster-comment-service/src/components/TodoList.tsx
+++ ts/cluster-comment-service/src/components/TodoList.tsx
@@ -1,18 +1,45 @@
 "use client";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../store/store";
+import { updateTodo, deleteTodo } from "../store/slices/todoSlice";
 import { Todo } from "../models/Todo";
 
 const TodoList = () => {
   const todosObj = useSelector((state: RootState) => state.todos.todos);
   const todos: Todo[] = Object.values(todosObj);
+  const dispatch = useDispatch();
 
   return (
     <ul>
       {todos.map((todo) => (
         <li key={todo.id}>
-          <strong>{todo.title}</strong>
-          <div>{todo.description}</div>
+          <input
+            value={todo.title}
+            onChange={(e) =>
+              dispatch(
+                updateTodo({
+                  ...todo,
+                  title: e.target.value,
+                }),
+              )
+            }
+          />
+          <div>
+            <textarea
+              value={todo.description ?? ""}
+              onChange={(e) =>
+                dispatch(
+                  updateTodo({
+                    ...todo,
+                    description: e.target.value,
+                  }),
+                )
+              }
+            />
+          </div>
+          <button onClick={() => dispatch(deleteTodo(todo.id))}>
+            delete todo
+          </button>
         </li>
       ))}
     </ul>
